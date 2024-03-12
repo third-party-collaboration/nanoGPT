00b8403aafa10d6875dc6bb9df081f6055f8b6bb
denezt <denezt@yahoo.com>
2024-03-12 21:25:55 +0100

    Adding a new training configuration

---
+++ data/learning/readme.md
@@ -0,0 +1,9 @@
+
+# Bash For Professional
+
+Bash For Professional, of the rnn.
+
+After running `prepare.py`:
+
+- train.bin has 1,003,854 tokens
+- val.bin has 111,540 tokens
--- examples.sh
+++ examples.sh
@@ -4,8 +4,6 @@
 # python train.py config/train_shakespeare_char.py --compile=False --eval_iters=20 --log_interval=1 --block_size=256 \
 # --batch_size=64 --n_layer=4 --n_head=4 --n_embd=768 --max_iters=5000 --lr_decay_iters=200 --dropout=0.2
 
-# Better Run with more little accuracy
-
 option=$1
 export TORCH_USE_CUDA_DSA=1
 export CUDA_LAUNCH_BLOCKING=1
@@ -19,9 +17,8 @@ export CUDA_LAUNCH_BLOCKING=1
 #esac
 
 case $option in
-	-s|--start)  CUDA_VISIBLE_DEVICES=0,1 torchrun train.py config/train_shakespeare_char.py --compile=False --eval_iters=20 --log_interval=1 --batch_size=12 \
-	--n_layer=6 --n_head=6 --n_embd=840 --max_iters=5000 \
-	--lr_decay_iters=5000 --dropout=0.1
+	-s|--start) CUDA_VISIBLE_DEVICES=0,1 torchrun train.py config/train_bash_notes.py --compile=False --eval_iters=20 --log_interval=10 \
+	--batch_size=12 --n_layer=6 --n_head=6 --n_embd=840 --max_iters=5000 --lr_decay_iters=5000 --dropout=0.1
 	;;
 	-k|--kill) for i in {0..9}; do sudo kill -9 $(pgrep torchrun); printf "iter: $i\n"; done;;
 esac
